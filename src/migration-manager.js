0fff93dce62c55a9a375f4b8e442c5cc129cde35
Javokhir <javokhir.akramjonov@gmail.com>
2025-04-17 10:06:15 +0500

    use env for accessing credentials for db

--- src/migration-manager.js
+++ src/migration-manager.js
@@ -4,11 +4,11 @@ import fs from 'fs/promises';
 
 const sequelize = new Sequelize({
     dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'password',
-    database: 'test_db',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
 });
 
 const MIGRATION_EXTENSIONS = {
